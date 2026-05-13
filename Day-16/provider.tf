0d713999d5dfac26a469552e186f78d01a01f88d
dhi98pat <dhirajpatel302@gmail.com>
2026-05-13 16:23:45 +0530

    updating the tf fils

---
+++ Day-16/provider.tf
@@ -0,0 +1,17 @@
+# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
